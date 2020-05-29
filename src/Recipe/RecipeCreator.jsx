066f63f38e08b7a109c67ae9403c3c56aa40fc61
titan00001 <gplay9792@gmail.com>
2020-05-29 21:55:54 +0530

    Wrapped all received data into a single json

--- src/Recipe/RecipeCreator.jsx
+++ src/Recipe/RecipeCreator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import FeedbackDiv from '../components/feedback'
 // import dosa from '../testRecipe'
 import Form from '../components/form';
@@ -6,33 +6,49 @@ import Form from '../components/form';
 function RecipeCreator() {
 
   const [ping, setPing] = useState(false);
+  const [count, setCount] = useState(0);
+  const [input, ] = useState({"name":"", "author":"", "date":"", "description":"", "tips":""});
 
 
   const callbackFunction = (data) => {
-    // console.log("bye");
-    console.log(data);
+
+    input[data.name] = data.content;
+
+    setCount(count+1);
+
     setPing(false);
 
   }
 
-  const handleData = () => {
-    // console.log("hello")
+  const handleClick = () => {
+    
+    setCount(count+1);
     setPing(true);
   }
+
+  const handleChange = (name, value) => {
+    input[name] = value;
+  }
+
+  useEffect(() => {
+    if(count === 2){
+      console.log(input);
+    }
+  },[count, input])
 
 
   return (
     <div>
       <label>Name of Dish</label>
-      <input type="text" id="name"/>
+      <input type="text" name="name" onChange={(e) => {handleChange('name', e.target.value)}}/>
       <label>Chef</label>
-      <input type="text" id="author"/>
+      <input type="text" name="author" onChange={(e) => {handleChange('author', e.target.value)}}/>
       <label>Date</label>
-      <input type="date" id="date"/>
+      <input type="date" name="date" onChange={(e) => {handleChange("date", e.target.value)}}/>
 
       <div>
         <h3>Description</h3>
-        <textarea name="description" cols="30" rows="3" placeholder="Any Description"></textarea>
+        <textarea name="description" cols="30" rows="3" placeholder="Any Description" onChange={(e) => {handleChange("description", e.target.value)}}></textarea>
       </div>
 
       <div>
@@ -52,10 +68,10 @@ function RecipeCreator() {
 
       <div>
         <h3>Tips</h3>
-        <textarea name="tips" cols="30" rows="10" placeholder="Any Tips"></textarea>
+        <textarea name="tips" cols="30" rows="10" placeholder="Any Tips" onChange={(e) => {handleChange("tips", e.target.value)}}></textarea>
       </div>
 
-      <button onClick = {(e) => {handleData()}}>Display Tags</button>
+      <button onClick = {(e) => {handleClick()}}>Display Tags</button>
 
     </div>
   );
